bca2a09069131447a5a820520ee3937832739eb8
Suru <garudahorror25@gmail.com>
2022-10-12 18:15:24 -0400

    Finished

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const $container = document.querySelector('.container');
+const $buttonReset = document.querySelector('.reset');
+let size = 128;
 
 function changeBgColor() {
-  this.style.backgroundColor = `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`;
-}
 
-function renderGrid(tag, cont, num) {
-  for (let i = 0; i < num; i++) {
-    const elm = document.createElement(tag);
-    elm.addEventListener('mouseenter', changeBgColor);
-    cont.appendChild(elm);
+  if (this.dataset.count < 10) {
+    this.style.backgroundColor = `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`;
+    this.dataset.count = ++this.dataset.count;
+    console.log(this.dataset.count);
+  } else {
+    this.style.backgroundColor = '#000'
   }
 }
 
-renderGrid('div', $container, 32);
+const resetColors = () => {
+  elements = $container.querySelectorAll('div');
+  elements.forEach(elm => {
+    elm.style.backgroundColor = '#fff'
+    elm.dataset.count = '0'
+  });
+}
+
+
+$buttonReset.addEventListener('click', resetColors)
+
+function renderGrid(num) {
+  for (let i = 0; i < num; i++) {
+    const elm = document.createElement('div');
+    elm.addEventListener('mouseenter', changeBgColor);
+    // elm.addEventListener('click', changeBgColor);
+    elm.dataset.count = '0';
+    $container.appendChild(elm);
+  }
+}
+
+renderGrid(size);
